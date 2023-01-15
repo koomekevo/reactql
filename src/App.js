dbb70cc9b14d5603235ccb1573b6fc926a5e5328
koomekevo <koome.kevo@gurucrafts.net>
2023-01-15 19:16:17 +0300

    Add ApolloProvider to index.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 function App() {
-  const client = new ApolloClient({
-    uri: "https://api.spacex.land/graphql",
-    cache: new InMemoryCache(),
-  });
-
   return (
     <div className="App">
       <header className="App-header">
